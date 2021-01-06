68080836229604d9493364b602046f5f3d5e7f24
sumeetmohite <sumeetmohite9@gmail.com>
2021-01-06 18:50:10 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import "./styles/App.scss";
+import Song from "./components/Song";
+import Player from "./components/Player";
+import data from "./util";
+
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[4]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Song currentSong = {currentSong}/>
+      <Player/>   
+
     </div>
   );
 }
---
+++ src/components/Player.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faPlay, faAngleLeft, faAngleRight} from '@fortawesome/free-solid-svg-icons';
+
+
+const Player= () => {
+    return (
+        <div className = "Player">
+            <div className="time-control">
+                <p>Start Time</p>
+                <input type = "range">
+                </input>
+                <p>End Time</p>
+            </div>
+            <div className="play-control">
+                <FontAwesomeIcon className ="skip-back" size = "2x" icon = {faAngleLeft}></FontAwesomeIcon>
+                <FontAwesomeIcon className = "play" size = "2x" icon = {faPlay}></FontAwesomeIcon>
+                <FontAwesomeIcon className ="skip-forward" size = "2x" icon = {faAngleRight}></FontAwesomeIcon>
+            </div>
+            
+            
+        </div>
+    )
+}
+
+export default Player;
