f4512cdf1880023373aa7cb09a2884852d9a7583
sumeetmohite <sumeetmohite9@gmail.com>
2021-01-18 20:55:12 +0530

    songs

--- src/data.js
+++ src/data.js
@@ -1,68 +1,66 @@
-import { v4 as uuidv4 } from "uuid";
+import {v4 as uuidv4} from "uuid"; 
+
 function chillHop() {
-  return [
-    {
-      name: "Beaver Creek",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
-      color: ["#205950", "#2ab3bf"],
-      id: uuidv4(),
-      active: true,
-    },
-    {
-      name: "Daylight",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-      artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
-      color: ["#EF8EA9", "#ab417f"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Keep Going",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-      artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
-      color: ["#CD607D", "#c94043"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Nightfall",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-      artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
-      color: ["#EF8EA9", "#ab417f"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Reflection",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
-      artist: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
-      color: ["#CD607D", "#c94043"],
-      id: uuidv4(),
-      active: false,
-    },
-    {
-      name: "Under the City Stars",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
-      color: ["#205950", "#2ab3bf"],
-      id: uuidv4(),
-      active: false,
-    },
-    //ADD MORE HERE
-  ];
+    return ([
+        {   name: "Ocean View",
+            artist: "G Mills, Kyle McEvoy, Luke Otwell",
+            cover: "https://chillhop.com/wp-content/uploads/2020/10/0e5bb63f838ff92eeac142aae944e9f678df13c9-1024x1024.jpg",
+            color: ["#CAB1C7","#61507C"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=10320",
+            id: uuidv4(),
+            active: true,
+        },
+        {   name: "Frozen in Time",
+            artist: "Kendall Miles",
+            cover: "https://chillhop.com/wp-content/uploads/2020/10/0e5bb63f838ff92eeac142aae944e9f678df13c9-1024x1024.jpg",
+            color: ["#CAB1C7","#61507C"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=10279",
+            id: uuidv4(),
+            active: false,
+        },
+        {   name: "Sugarless",
+            artist: "The Field Tapes, Aviino",
+            cover: "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
+            color: ["#B4C1A5","#DBE8DF"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=11243",
+            id: uuidv4(),
+            active: false,
+        },
+        {   name: "Through Trees",
+            artist: "Sleepy Fish, The Field Tapes",
+            cover: "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg",
+            color: ["#B4C1A5","#DBE8DF"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=11251",
+            id: uuidv4(),
+            active: false,
+        },
+        {   name: "Firefly Field",
+            artist: "Aso, Middle School, Aviino",
+            cover: "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+            color: ["#175049","#34B3A6",],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=10078",
+            id: uuidv4(),
+            active: false,
+        },
+        {   name: "Hereafter",
+            artist: "Makzo",
+            cover: "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
+            color: ["#5F6FA9","#F3A864"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=11770",
+            id: uuidv4(),
+            active: false,
+        },
+        {   name: "After Dark",
+            artist: "Sugi.wa",
+            cover:"https://chillhop.com/wp-content/uploads/2020/12/7e98d3028a22ee7f16f6a9bfcdc2089f089777a5-1024x1024.jpg",
+            color: ["#2D3673","#723766"],
+            audio: "https://mp3.chillhop.com/serve.php/?mp3=8105",
+            id: uuidv4(),
+            active: false,
+        }
+    ]
+    
+    )
 }
 
 export default chillHop;
